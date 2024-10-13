ba4475e085c75927ac968231cc5f0016fbcf204c
Harshith M R <harsh.reactdev@gmail.com>
2024-10-14 02:16:09 +0530

    chore: day 5 - print, comments and escape sequence

---
+++ day5.py
@@ -0,0 +1,22 @@
+'''
+    asdjhbasd
+    asdsjhbasd
+    asdkbhasdjhb 
+    These are multi line comments
+'''
+
+# this is a single line comment
+
+# -----------------------------------------------------------------------
+
+print('Hey it\'s me, harsh.!') #here in order to use single quote in between the string i have used an escape sequence character, i.e., a back slash followed by the character i wanted, i.e., single quote.
+
+print('hi, my name is harsh.! \nLets code.!') #escape sequence to add a new line is '\n'
+
+# -----------------------------------------------------------------------
+
+print('hi', 5, 6, 5+6) #print accepts multiple values
+
+print('hi', 5, 6, 5+6, sep='-') #print accepts a parameter called sep, which is a separator with which we want multiple values tht we give to print function to be separated
+
+print('hi', 5, 6, 5+6, sep='-', end='@harsh\n') #print also accepts another parameter called end, and it the character or string which will be appended to the printed value 
