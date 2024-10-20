27dfba088267544a1578bc837c21189c63b8dbc8
Harshith M R <harsh.reactdev@gmail.com>
2024-10-20 12:45:41 +0530

    chore: variables and data types

---
+++ day6.py
@@ -0,0 +1,32 @@
+# Variables and data types
+
+a = 3
+b = 'hey'
+c = True
+d = False
+e = 0.123
+f = None
+
+# lists, tuples and dict
+g = [1, True, 'hey', 0.23, None, [1, 2, 3], {"a": 2}]
+
+h = tuple([1, 2, 4])
+# print(h)
+# print(type(h))
+# h[1] = 5 //tuples are immutable
+
+
+# dictionary : type -> dict
+i = {
+    "name": 'harsh',
+    "age": 24,
+    "booksRead": ['boundaries', 'The art of seduction'],
+    "edu": {
+        "sslc": "91.5%",
+        "pre-uni": "80.5%",
+        "undergrad": "65%"
+    },
+    "isEmployed": False
+}
+
+# print(type(i))
