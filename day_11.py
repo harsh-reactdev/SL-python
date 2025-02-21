6dcbde93db644a945d4a4942032fd87441559104
Harshith M R <harsh.reactdev@gmail.com>
2025-02-21 23:58:02 +0530

    chore: day 11 - strings introduction

---
+++ day_11.py
@@ -0,0 +1,11 @@
+# Day 11
+# strings
+
+a = 'this is a string'
+b = "this is a string too, in double quotes"
+
+# below two are used for multi line strings where a new line is necessary
+c = """this too in triple double quotes"""
+d = '''i kid you not, but this one too, is a string, in triple single quotes/'''
+
+# ----------------------------------------------------------------------------------
